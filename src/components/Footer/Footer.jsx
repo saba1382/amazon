16e0e247acd50f53f973c0519e7f34f551e988cb
saeed moradi <ekbatan5111374@gmail.com>
2024-07-23 17:47:13 +0430

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import Logo from "../../assets/logo.png"
+import {
+    InboxIcon,
+    PhoneIcon,
+    LocationMarkerIcon,
+    LoginIcon,
+    UsersIcon,
+    LinkIcon,
+} from "@heroicons/react/outline"
+import css from './Footer.module.css'
+const Footer = () => {
+  return (
+    <div className={css.cFooterWrapper}>
+        <hr />
+
+        <div className={css.cFooter}>
+            <div className={css.logo}>
+                <img src={Logo} alt="" />
+                <span>amazon</span>
+            </div>
+
+            <div className={css.block}>
+                <div className={css.detail}>
+                    <span>Contact US</span>
+                    <span className={css.pngLine}>
+                        <LocationMarkerIcon className={css.icon} />
+                        <span>111 north avenue Orlando, FL 32801</span>
+                    </span>
+
+                     <span className={css.pngLine}>
+                        {""}
+                        <PhoneIcon className={css.icon} />
+                        <a href="tel:352-306-4415">325-306-4415</a>
+                     </span>
+
+                     <span className={css.pngLine}>
+                        <InboxIcon className={css.icon} />
+                        <a href="mailto:support@amazon.con">support@amazon.com</a>
+                     </span>
+                </div>
+            </div>
+
+            <div className={css.block}>
+                <div className={css.detail}>
+                    <span>Accont</span>
+                    <span className={css.pngLine}>
+                        <LoginIcon className={css.icon} />
+                        Sign In
+                    </span>
+                </div>
+            </div>
+
+            <div className={css.block}>
+                <div className={css.detail}>
+                    <span>Company</span>
+                    <span className={css.pngLine}>
+                        <UsersIcon className={css.icon} />
+                        About us
+                    </span>
+                </div>
+            </div>
+
+            <div className={css.block}>
+                <div className={css.detail}>
+                    <span>Resources</span>
+                    <span className={css.pngLine}>
+                        <LinkIcon className={css.icon} />
+                        <p>Safety Privacy & Terms</p>
+                    </span>
+                </div>
+            </div>
+,         </div>
+
+          <div className={css.copyRight}>
+            <span>Copyright 2022 by Amazon, Inc.</span>
+            <span>All rights reserved.</span>
+          </div>
+    </div>
+  )
+}
+
+export default Footer
